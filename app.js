9e0e6050ff23eca3534d0ba60b448d8b831a79cd
josh <kerush@163.com>
2020-07-31 17:35:22 +0800

    upadate

--- app.js
+++ app.js
@@ -5,6 +5,50 @@
 // console.log("Hello Joy-React");
 import { JoyReact, Component } from "./JoyReact";
 
+class Square extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      value:null
+    }
+  }
+  render(){
+    return(
+      <button className='square' onClick={()=> this.setState({value:3})}>
+        {this.state.value?this.state.value:''}
+      </button>
+    )
+  }
+}
+
+class Board extends Component {
+  renderSquare(i){
+    return <Square value={i} />
+  }
+  render(){
+    return(
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    )
+  }
+}
+
+
 class MyComponent extends Component {
   render() {
     return (
@@ -21,8 +65,6 @@ class MyComponent extends Component {
   }
 }
 
-let a = <MyComponent name="mpt" id="mpt">
-  <div>5566</div>
-</MyComponent>;
+let a = <Board />;
 
 JoyReact.render(a, document.querySelector('#app'));
